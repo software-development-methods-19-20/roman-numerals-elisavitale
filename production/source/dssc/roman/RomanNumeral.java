f4737ee936ed77bc27797d1949676de6b8aa4866
elisavitale <elisavitale@live.it>
2019-11-19 21:22:36 +0100

    We can now convert to Roman numerals any number smaller or equal than 5.

--- production/source/dssc/roman/RomanNumeral.java
+++ production/source/dssc/roman/RomanNumeral.java
@@ -1,12 +1,25 @@
 package dssc.roman;
 
+import java.util.HashMap;
+
 public class RomanNumeral {
 
+    private int decimal;
+
     public RomanNumeral(int decimal){
+        this.decimal = decimal;
     }
 
     @Override
     public String toString() {
-        return "";
+        if (decimal == 0) {
+            return "";
+        } else if (decimal < 4 ) {
+            return "I".repeat(decimal);
+        } else if (decimal == 4) {
+            return "I" + "V";
+        } else {
+            return "V";
+        }
     }
 }
